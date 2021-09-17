7587040b0b4d24dbd625568ce96819403f6bee09
twg <tachogardiner@gmail.com>
2021-09-17 14:34:52 -0700

    copypasted css

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,11 +1,20 @@
+import PropTypes from 'prop-types'
+
 const Header = ({title}) => {
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <button className='btn'>Add</button>
         </header>
     )
 }
+
 Header.defaultProps = {
     title: 'Task Tracker',
 }
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
 export default Header
