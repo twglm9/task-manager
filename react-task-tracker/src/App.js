1723a4e6c5b2c6853cb66d46deec2d3302bf91fa
twg <tachogardiner@gmail.com>
2021-09-17 14:27:51 -0700

    header component

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,7 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
+
   return (
     <div className="container"> 
-      <h1>Hello from React</h1>
+      <Header />
     </div>
   );
 }
