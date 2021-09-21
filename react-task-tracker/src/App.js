1490198af3393c45e131132e8dd97a32f3a7f281
twg <tachogardiner@gmail.com>
2021-09-21 10:38:54 -0700

    Implemented toggle reminder

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -28,12 +28,23 @@ function App() {
     //delete task
     const deleteTask = (id) => {
       setTasks(tasks.filter((task) => task.id !== id))
-      console.log("delete", id)
+      //console.log("delete", id)
     }
+
+    //toggle reminder
+    const toggleReminder = (id) => {
+      setTasks(tasks.map((task) => task.id === id ? 
+        { ...task, reminder: !task.reminder } 
+        : task ))
+      console.log(id)
+    }
+
     return (
       <div className="container"> 
         <Header />
-        {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ("No tasks to show")}
+          {tasks.length > 0 ? 
+            (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) 
+            : ("No tasks to show")}
       </div>
     );
 }
