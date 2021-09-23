31b53c82952f6cc28dd2d8161d291b92828b236e
twg <tachogardiner@gmail.com>
2021-09-23 15:01:37 -0700

    Made tasks submittable

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -25,8 +25,16 @@ function App() {
             reminder: true,
         },
     ]
-)
-    //delete task
+  )
+
+    //add task
+    const addTask = (task) => {
+      console.log(task)
+      const id = Math.floor(Math.random() * 1000) + 1
+      const newTask = { id, ...task }
+      setTasks([...tasks, newTask])
+    }
+  //delete task
     const deleteTask = (id) => {
       setTasks(tasks.filter((task) => task.id !== id))
       //console.log("delete", id)
@@ -43,7 +51,7 @@ function App() {
     return (
       <div className="container"> 
         <Header />
-        <AddTask />
+        <AddTask onAdd={addTask} />
           {tasks.length > 0 ? 
             (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) 
             : ("No tasks to show")}
